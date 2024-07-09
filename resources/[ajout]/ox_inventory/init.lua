cf562e44c67a46e50fe4c86a58e1c5101354c1c8
CtFatalv <97322636+CtFatalv@users.noreply.github.com>
2024-07-09 21:39:41 +0200

    Legacy City

--- resources/[ajout]/ox_inventory/init.lua
+++ resources/[ajout]/ox_inventory/init.lua
@@ -86,7 +86,7 @@ else
         itemnotify = GetConvarInt('inventory:itemnotify', 1) == 1,
         weaponnotify = GetConvarInt('inventory:weaponnotify', 1) == 1,
         imagepath = GetConvar('inventory:imagepath', 'nui://ox_inventory/web/images'),
-        dropprops = GetConvarInt('inventory:dropprops', 0) == 1,
+        dropprops = GetConvarInt('inventory:dropprops', 1) == 1,
         dropmodel = joaat(GetConvar('inventory:dropmodel', 'p_ld_bs_bag_01')),
         weaponmismatch = GetConvarInt('inventory:weaponmismatch', 1) == 1,
         ignoreweapons = json.decode(GetConvar('inventory:ignoreweapons', '[]')),
