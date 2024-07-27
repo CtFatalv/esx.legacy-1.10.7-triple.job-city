81d2d4d506c4799c691e2247d8e3caf74bcda1d3
CtFatalv <97322636+CtFatalv@users.noreply.github.com>
2024-07-27 13:10:53 +0200

    Legacy City

--- resources/[ajout]/ox_inventory/data/shops.lua
+++ resources/[ajout]/ox_inventory/data/shops.lua
@@ -11,6 +11,60 @@
 ---@field model? number[]
 
 return {
+	General = {
+		name = 'Epicerie Fine',		
+        blip = {
+			id = 402, colour = 69, scale = 2.7
+		},
+		inventory = {
+			{ name = 'burger', price = 10 },
+			{ name = 'water', price = 10 },
+		}, locations = {
+			vec3(25.7, -1347.3, 29.49),
+			vec3(-3038.71, 585.9, 7.9),
+			vec3(-3241.47, 1001.14, 12.83),
+			vec3(1728.66, 6414.16, 35.03),
+			vec3(1697.99, 4924.4, 42.06),
+			vec3(1961.48, 3739.96, 32.34),
+			vec3(547.79, 2671.79, 42.15),
+			vec3(2679.25, 3280.12, 55.24),
+			vec3(2557.94, 382.05, 108.62),
+			vec3(373.55, 325.56, 103.56),
+			vec3(1135.808, -982.281, 46.415),
+			vec3(-1222.915, -906.983, 12.326),
+			vec3(-1487.553, -379.107, 40.163),
+			vec3(-2968.243, 390.910, 15.043),
+			vec3(1166.024, 2708.930, 38.157),
+			vec3(1392.562, 3604.684, 34.980),
+			vec3(-1393.409, -606.624, 30.319),
+			vec3(-47.240, -1757.630, 29.420),
+			vec3(-706.69, -913.69, 19.22),
+			vec3(1164.15, -322.9, 69.21),
+			vec3(-1820.51, 793.77, 138.11)
+		}, targets = {
+			{ loc = vec3(25.06, -1347.32, 29.5), length = 0.7, width = 0.5, heading = 0.0, minZ = 29.5, maxZ = 29.9, distance = 1.5, debug = false },
+			{ loc = vec3(-3039.18, 585.13, 7.91), length = 0.6, width = 0.5, heading = 15.0, minZ = 7.91, maxZ = 8.31, distance = 1.5, debug = false },
+			{ loc = vec3(-3242.2, 1000.58, 12.83), length = 0.6, width = 0.6, heading = 175.0, minZ = 12.83, maxZ = 13.23, distance = 1.5, debug = false },
+			{ loc = vec3(1728.39, 6414.95, 35.04), length = 0.6, width = 0.6, heading = 65.0, minZ = 35.04, maxZ = 35.44, distance = 1.5, debug = false },
+			{ loc = vec3(1698.37, 4923.43, 42.06), length = 0.5, width = 0.5, heading = 235.0, minZ = 42.06, maxZ = 42.46, distance = 1.5, debug = false },
+			{ loc = vec3(1960.54, 3740.28, 32.34), length = 0.6, width = 0.5, heading = 120.0, minZ = 32.34, maxZ = 32.74, distance = 1.5, debug = false },
+			{ loc = vec3(548.5, 2671.25, 42.16), length = 0.6, width = 0.5, heading = 10.0, minZ = 42.16, maxZ = 42.56, distance = 1.5, debug = false },
+			{ loc = vec3(2678.29, 3279.94, 55.24), length = 0.6, width = 0.5, heading = 330.0, minZ = 55.24, maxZ = 55.64, distance = 1.5, debug = false },
+			{ loc = vec3(2557.19, 381.4, 108.62), length = 0.6, width = 0.5, heading = 0.0, minZ = 108.62, maxZ = 109.02, distance = 1.5, debug = false },
+			{ loc = vec3(373.13, 326.29, 103.57), length = 0.6, width = 0.5, heading = 345.0, minZ = 103.57, maxZ = 103.97, distance = 1.5, debug = false },
+			{ loc = vec3(1134.9, -982.34, 46.41), length = 0.5, width = 0.5, heading = 96.0, minZ = 46.4, maxZ = 46.8, distance = 1.5, debug = false },
+			{ loc = vec3(-1222.33, -907.82, 12.43), length = 0.6, width = 0.5, heading = 32.7, minZ = 12.3, maxZ = 12.7, distance = 1.5, debug = false },
+			{ loc = vec3(-1486.67, -378.46, 40.26), length = 0.6, width = 0.5, heading = 133.77, minZ = 40.1, maxZ = 40.5, distance = 1.5, debug = false },
+			{ loc = vec3(-2967.0, 390.9, 15.14), length = 0.7, width = 0.5, heading = 85.23, minZ = 15.0, maxZ = 15.4, distance = 1.5, debug = false },
+			{ loc = vec3(1165.95, 2710.20, 38.26), length = 0.6, width = 0.5, heading = 178.84, minZ = 38.1, maxZ = 38.5, distance = 1.5, debug = false },
+			{ loc = vec3(1393.0, 3605.95, 35.11), length = 0.6, width = 0.6, heading = 200.0, minZ = 35.0, maxZ = 35.4, distance = 1.5, debug = false },
+			{ loc = vec3(-47.24, -1757.63, 29.42), length = 0.6, width = 0.6, heading = 320.0, minZ = 29.42, maxZ = 29.82, distance = 1.5, debug = false },
+            { loc = vec3(-706.69, -913.69, 19.22), length = 0.4, width = 0.5, heading = 0.0, minZ = 19.22, maxZ = 19.62, distance = 1.5, debug = false },
+			{ loc = vec3(1164.15, -322.9, 69.21), length = 0.4, width = 0.45, heading = 10.0, minZ = 69.21, maxZ = 69.61, distance = 1.5, debug = false },
+			{ loc = vec3(-1820.5, 793.7, 138.1), length = 0.4, width = 0.4, heading = 313.0, minZ = 138.06, maxZ = 138.51, distance = 1.5, debug = false }
+		}
+	},
+
 	YouTool = {
 		name = 'YouTool',
 		blip = {
